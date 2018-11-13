b60dc1ad9fd80c61ece492bcb227cde9306fa9a6
Bhavesh Sharma <bhaveshbs789@gmail.com>
2018-11-13 21:46:33 +0530

    sw.js file updated with the install stage code for SW

--- sw.js
+++ sw.js
@@ -1,1 +1,36 @@
-console.log('Hi , i am serice worker');
+//console.log('Hi , i am serice worker');
+
+let cacheName = 'restaurant-app-stage-1';
+let urlsToCache = [
+  './',
+  './index.html',
+  './restaurant.html',
+  './css/styles.css',
+  './js/main.js',
+  './js/restaurant_info.js',
+  './js/dbhelper.js',
+  './data/restaurants.json',
+  './img/1.jpg',
+  './img/2.jpg',
+  './img/3.jpg',
+  './img/4.jpg',
+  './img/5.jpg',
+  './img/6.jpg',
+  './img/7.jpg',
+  './img/8.jpg',
+  './img/9.jpg',
+  './img/10.jpg'
+];
+
+self.addEventListener('install', function(evt){
+	evt.waitUntil(
+		caches.open(cacheName)
+		.then(function(cache){
+			console.log('Cache Opened');
+			return cache.addAll(urlsToCache);
+		}).catch(function(err){
+			console.log(err);
+		})
+	);
+});
+
